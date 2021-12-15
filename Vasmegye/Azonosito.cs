99d695e3588e1dcf599d915c47d90da9ba3a724d
jozsiegykecske <jozsiegykecske@gmail.com>
2021-12-15 14:51:02 +0100

    Add project files.

---
+++ Vasmegye/Azonosito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasmegye
+{
+  internal class Azonosito
+  {
+    public int nem { get; set; }
+    public int szuletes { get; set; }
+    public int megkulonbozteto { get; set; }
+    public Azonosito(string be)
+    {
+      string[] a = be.Split('-');
+      nem = Convert.ToInt32(a[0]);
+      szuletes = Convert.ToInt32(a[1]);
+      megkulonbozteto = Convert.ToInt32(a[2]);
+    }
+  }
+    
+}
---
+++ Vasmegye/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vasmegye
+{
+  internal class Program
+  {
+    static int osszes = 0;
+    static int db = 0;
+    static List<Azonosito> lista = new List<Azonosito>();
+    static void Main(string[] args)
+    {
+      Beolvasas();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void HatodikFeladat()
+    {
+      Console.WriteLine($"6.feladat: A fiú csecsemők száma: {db} db");
+    }
+
+    private static void OtodikFeladat()
+    {
+      Console.WriteLine($"5. feladat: {lista.Count} db csecsemő született!");
+    }
+
+    private static void NegyedikFeladat()
+    {
+      Console.WriteLine("3.feladat:");
+      for (int i = 0; i < lista.Count; i++)
+      {
+        if (!CdvEll(lista[i]))
+        {
+          Console.WriteLine($"\tHibás a {lista[i].nem}-{lista[i].szuletes}-{lista[i].megkulonbozteto} személyi azonosító");
+          lista.RemoveAt(i);
+        }
+      }
+    }
+
+    private static void Beolvasas()
+    {
+      using (StreamReader be = new StreamReader("vas.txt"))
+      {
+        while (!be.EndOfStream)
+        {
+          lista.Add(new Azonosito(be.ReadLine()));
+        }
+      }
+      osszes = lista.Count();
+    }
+    public static bool CdvEll(Azonosito be)
+    {
+      int ossz = 0;
+      string a = be.nem.ToString() + be.szuletes.ToString() + be.megkulonbozteto.ToString();
+      for (int i = 0; i < a.Length-1; i++)
+      {
+        ossz += Convert.ToInt32(a[i]) * (10 - i);
+      }
+      if (be.nem == 1 || be.nem == 3)
+      {
+        db++;
+      }
+      if (Convert.ToInt32(a.Substring(a.Length-1))==(ossz%11))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+      
+    }
+  }
+}
